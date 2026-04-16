5edb27aa6bf66e5d61e6191628a1e6baa638f354
Lithe-1 <nijacobs0910@gmail.com>
2026-04-15 23:44:53 -0500

    Core pipeline layout
    Leiden implementation issues and graph value changes need to be made for dimensionality. Clustering and differential expression functional. Umap changes required.

--- scRNA_analysis/scRNA_analysis.py
+++ scRNA_analysis/scRNA_analysis.py
@@ -28,7 +28,36 @@ def run_scrna_pipeline():
     # Pre filtered QC metrics Visualization
     sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True, show=False)
     plt.savefig('qc_metrics_violin.png')
+    print("Normalizing and identifying variable genes...")
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 
     #Saves raw data
     adata.raw= adata
     adata = adata[:, adata.var.highly_variable].copy()
+
+    sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
+    sc.tl.leiden(adata, resolution=0.5)
+    print("Differential expression analysis...")
+    sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
+
+    marker_map = {
+        "0": "CD4 T-cells", "1": "B-cells", '2':' CD14+ Monocytes', '3':'NK cells',
+        '4':'CD8 T-cells', '5':'FCGR3A+ Monocytes', '6':'Dendritic cells', 
+        '7':'platelets'
+    }
+    #edge case for when leiden clustering does not yield 8 clusters
+    adata.obs['cell_type']=adata.obs['leiden'].map(lambda x: marker_map.get(x, f'cluster {x}'))
+
+
+    print('Creating visuals')
+    sc.pl.umap(adata, color=['cell_type', 'doublet_scores'],
+        title=['Cell Type annotations', 'Doublet Scores'],
+        frameon=False, legend_loc='on data', save='umap_celltype_doublets.png')  
+
+    sc.pl.rank_genes_groups_dotplot(adata, n_genes=3, values_to_plot='logfoldchanges',
+                                    min_logfoldchange=3, vmax=7, vmin=7, cmap ='bwr', save='_markers.png')
+
+if __name__ == "__main__":
+    processed_adata = run_scrna_pipeline()          
